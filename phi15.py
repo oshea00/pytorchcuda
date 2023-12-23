177bf0f3e3f17e30fcb7506845ece7385cf612b1
Mike OShea <oshea00@gmail.com>
2023-12-22 22:27:05 -0800

    add some time marks

--- phi15.py
+++ phi15.py
@@ -3,10 +3,16 @@ import time
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+def markTime(start,msg):
+    print(f"{msg} {time.time() - start}")
+
+start=time.time()
+
 torch.set_default_device("cuda")
 
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", torch_dtype="auto", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
+markTime(start,"load model")
 
 inputs = tokenizer('''def fizzbuzz:
    """
@@ -14,9 +20,8 @@ inputs = tokenizer('''def fizzbuzz:
    """
    """''', return_tensors="pt", return_attention_mask=False)
 
-start=time.time()
 outputs = model.generate(**inputs, max_length=200)
+markTime(start,"generate")
 text = tokenizer.batch_decode(outputs)[0]
-end=time.time()
-print(text)
-print(end - start)
+markTime(start,"decode")
+print(f"Total time: {time.time() - start}")
