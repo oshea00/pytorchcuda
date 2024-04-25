a4fc823d034ba4aa95a25e3937070ea1a3bf69f3
Mike OShea <oshea00@gmail.com>
2024-04-25 16:04:19 -0700

    llama3 and langchain

---
+++ transformers_llama3.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+# Use a pipeline as a high-level helper
+# pipeline will look for HF_TOKEN env variable
+# to authenticate with Hugging Face.
+pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B")
+output = pipe("Hey how are you doing today?")
+print(output[0]['generated_text'])
