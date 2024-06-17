5bf6e61469fee21e9d91e39da09e76ef7bd6e49a
Mike OShea <oshea00@gmail.com>
2024-06-17 16:30:09 -0700

    streamed chat loop

--- ollama_openai.py
+++ ollama_openai.py
@@ -1,20 +1,35 @@
 from openai import OpenAI
 
 client = OpenAI(
-    base_url='http://localhost:11434/v1',
+    base_url='https://ollama.limpidfox.net/v1',
     # required but ignored
     api_key='ollama'
 )
 
-completion = client.chat.completions.create(
-  model="llama3",
-  messages=[{"role":"user","content":"What can I see at NVIDIA's GPU Technology Conference?"}],
-  temperature=0.5,
-  top_p=1,
-  max_tokens=1024,
-  stream=True
-)
+def answer_question(question):
+  completion = client.chat.completions.create(
+    model="llama3:latest",
+    messages=[{"role":"user","content":question}],
+    temperature=0.5,
+    top_p=1,
+    max_tokens=1500,
+    stream=True
+  )
 
-for chunk in completion:
-  if chunk.choices[0].delta.content is not None:
-    print(chunk.choices[0].delta.content, end="")
+  return completion
+
+# take a prompt from the console process with answer_question.
+# if the question is "exit", break the loop
+while True:
+  try:
+    question = input("Ask a question ('exit' to quit): ")
+    if question == "exit":
+      break
+    completion = answer_question(question)
+    for chunk in completion:
+      if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end='')
+    print()
+  except Exception as e:
+    print()
+    break    
