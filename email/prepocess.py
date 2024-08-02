6fa8e339caf6534c82930dea94c3bbed0944ee3e
Mike OShea <oshea00@gmail.com>
2024-08-01 20:01:17 -0700

    added presidio

--- email/prepocess.py
+++ email/prepocess.py
@@ -1,5 +1,55 @@
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
 import openai
+import spacy
 import sys
+
+# Initialize the Presidio Analyzer
+# use the small model to reduce the memory usage
+# download the mode as a file from https://spacy.io/usage/models#usage-import
+# With external URL
+#pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0-py3-none-any.whl
+#pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0.tar.gz
+
+# Using spacy info to get the external URL
+#pip install $(spacy info en_core_web_sm --url)
+
+# With local file
+#pip install /Users/you/en_core_web_sm-3.0.0-py3-none-any.whl
+#pip install /Users/you/en_core_web_sm-3.0.0.tar.gz
+
+nlp = spacy.load("en_core_web_sm")
+print (nlp._path)
+
+from presidio_analyzer.nlp_engine import NlpEngineProvider
+
+# Define the NLP engine and load the small spaCy model
+provider = NlpEngineProvider(nlp_configuration={"nlp_engine_name": "spacy", "models": [{"lang_code": "en", "model_name": "en_core_web_sm"}]})
+nlp_engine = provider.create_engine()
+
+# Initialize the AnalyzerEngine with the specified NLP engine
+analyzer = AnalyzerEngine(nlp_engine=nlp_engine)
+anonymizer = AnonymizerEngine()
+
+entities = ['PERSON', 'ORGANIZATION', 'EMAIL_ADDRESS', 'CREDIT_CARD', 'US_SSN', 'AU_ABN', 'IN_AADHAAR', 'US_DRIVER_LICENSE', 'US_PASSPORT', 
+            'AU_TFN', 'US_BANK_NUMBER', 'EMAIL', 'PHONE_NUMBER', 'URL', 'CRYPTO', 'AU_ACN']
+
+# Function to detect and mask PII in text
+def mask_pii(text):
+    """
+    Masks personally identifiable information (PII) in the given text.
+
+    Args:
+        text (str): The text containing PII.
+
+    Returns:
+        str: The anonymized text with PII masked.
+
+    """
+    results = analyzer.analyze(text=text, entities=entities, language='en')
+    anonymized_text = anonymizer.anonymize(text=text, analyzer_results=results)
+    return anonymized_text.text
+
 
 # Initialize the OpenAI API key
 client = openai.OpenAI()
@@ -9,7 +59,7 @@ def load_email_text(file_path):
         return file.read()
 
 def clean_email_content(file_path):
-    email_text = load_email_text(file_path)
+    email_text = mask_pii(load_email_text(file_path))
     
     # Define the prompt
     prompt = f"""
@@ -49,5 +99,5 @@ if __name__ == "__main__":
             print(cleaned_email)
             print("********************************************************")
             i+=1
-    except:
-        pass
+    except Exception as e:
+        print(e)
