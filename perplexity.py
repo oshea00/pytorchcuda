cd0f5c84a62dc9203c688fe1356c68ab14c3e03a
Mike OShea <oshea00@gmail.com>
2024-11-24 09:17:59 -0800

    perplexity example

---
+++ perplexity.py
@@ -0,0 +1,39 @@
+import os
+from openai import OpenAI
+
+PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY")
+
+messages = [
+    {
+        "role": "system",
+        "content": (
+            "You are an artificial intelligence assistant and you need to "
+            "engage in a helpful, detailed, polite conversation with a user."
+        ),
+    },
+    {   
+        "role": "user",
+        "content": (
+            "How many stars are in the universe?"
+        ),
+    },
+]
+
+client = OpenAI(api_key=PERPLEXITY_API_KEY, base_url="https://api.perplexity.ai")
+
+# chat completion without streaming
+response = client.chat.completions.create(
+    model="llama-3.1-sonar-large-128k-online",
+    messages=messages,
+)
+print(response)
+
+# chat completion with streaming
+#response_stream = client.chat.completions.create(
+#    model="llama-3.1-sonar-large-128k-online",
+#    messages=messages,
+#    stream=True,
+#)
+#for response in response_stream:
+#    print(response)
+
