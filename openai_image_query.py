d2f1bfbcbc087955ce3683e9961bdae99085f403
Mike O'Shea <moshea@paraport.com>
2025-03-04 18:21:18 -0800

    openai image query

--- openai_image_query.py
+++ openai_image_query.py
@@ -2,10 +2,9 @@ from openai import OpenAI
 import base64
 
 
-apikey = "eyJraWQiOiI3RVljV2sxX1UyWXdWWTh4Y19feDFWQ3hhR3dWMlVyRDYxMGhrZWh2amZjIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULi04UEM1NXhHMEcxUERLSGs1TXUzN21yLURTSlN1R1psM05qVWJoeGtIWDAiLCJpc3MiOiJodHRwczovL2VhdG9udmFuY2VkZXYub2t0YXByZXZpZXcuY29tL29hdXRoMi9hdXMyOHMxaDh0NTk5NXMzajBoOCIsImF1ZCI6InJhZGl1c2FpLXNlcnZpY2UiLCJpYXQiOjE3NDExNDA4MjQsImV4cCI6MTc0MTE0NDQyNCwiY2lkIjoiMG9hMmMxYmxvYXNwb0JQdE4waDgiLCJ1aWQiOiIwMHVyaHJmMGQ0MXdNME1iZTBoNyIsInNjcCI6WyJSQURJVVNBSV9DT01QTEVUSU9OIl0sImF1dGhfdGltZSI6MTc0MTE0MDgyMiwic3ViIjoiTWljaGFlbE9AcGFyYXBvcnQuY29tIn0.KufkGpG58EfdrFRgePyadgugUM865vfCeuPmf3EpvW28QAaoYF4bXKDEmwlgwHpAllwXN1ZqUQBff_X735WF-VMmJJnScKgrtgeBONSHzxC3DZF21SUdkFYaZkSrv8k9ZrmPTJQek-OrETvjvSGs3l5Tr48AA_jgonDj8Im6ytAx6g1HqnJ8tOzoMPsnJiPd3O4ldsoyOSRwNekDFtvMC1CFje3J3eCUeTe81LopvvQYp30bMaNLHGbfWWF-jBJ0_8YmvIDZQ7O1wZvyQlDNd4KzMU8HHcvmymRtRNIBwRa3XvgY6rxH8-Bz1nUNdvs7oWGB_6hr4i2bg8cupcZ4sA"
+apikey = "<enter>"
 client = OpenAI(
-#    base_url="https://api-uat.morganstanley.com/wm-aidp/aigateway/v1/openai/v1",
-    base_url="https://radai-dev.sandbox.genai.use1.aws.paraport.com/v1",
+    base_url="https://api.x.ai/v1/",
     api_key=apikey
 )
 
