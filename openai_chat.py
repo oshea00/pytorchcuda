895b8060cb463b7459186e10d3173464a84efb82
Mike OShea <oshea00@gmail.com>
2025-03-22 10:04:58 -0700

    openai_chat chg

--- openai_chat.py
+++ openai_chat.py
@@ -7,22 +7,25 @@ client = OpenAI(api_key=apikey)
 
 # create a multi-line prompt
 prompt = """
-Hello chatgpt, are you ready to work?
+Hello chatgpt, are you ready to work? Can you describe general relativity?
 """
 
+#MODEL = "gpt-4.5-preview"
+MODEL = "gpt-4o"
+
 completion = client.chat.completions.create(
-  model="gpt-4o",
+  model=MODEL,
   messages=[{"role":"user","content":f"{prompt}"}],
-  temperature=0.5,
-  top_p=1,
-  max_tokens=4096,
-  stream=False
+  temperature=0,
+  stream=True
 )
 
 # Serialize the completion object to JSON
-completion_json = completion.model_dump_json(indent=2)
-print(completion_json)
+# completion_json = completion.model_dump_json(indent=2)
+# print(completion_json)
 
-# for chunk in completion:
-#   if chunk.choices[0].delta.content is not None:
-#     print(chunk.choices[0].delta.content, end="")
+for chunk in completion:
+  if chunk.choices and chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
+
+# print(completion.choices[0].message.content)
